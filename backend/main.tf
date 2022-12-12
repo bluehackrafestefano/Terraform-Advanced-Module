2c89267a8e24b0974382c1ed18ff459a9d0dd84a
Rafe <stefanorafe@gmail.com>
2022-12-12 09:58:49 +0100

    updated backend version

--- backend/main.tf
+++ backend/main.tf
@@ -1,12 +1,12 @@
 module "s3_backend" {
   source  = "bluehackrafestefano/s3-backend/aws"
-  version = "1.0.0"
+  version = "2.0.0"
 }
 
 output "s3_backend_bucket_name" {
-  value = module.s3-backend.s3_backend_name
+  value = module.s3_backend.s3_backend_name
 }
 
 output "aws_dynamodb_table" {
-  value = module.s3-backend.dynamodb_table
+  value = module.s3_backend.dynamodb_table
 }
